a7b0eff5431095748e414bd3ed74603e41c3808b
lansheng228 <lansheng228@gmail.com>
2019-03-22 18:46:29 +0800

    修改内存占用测试方式

--- src/main/java/com/example/SurveyMain.java
+++ src/main/java/com/example/SurveyMain.java
@@ -1,44 +1,30 @@
 package com.example;
 
 import com.google.common.collect.EvictingQueue;
+import org.apache.lucene.util.RamUsageEstimator;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.IntStream;
 
 //测试内存消耗
 public class SurveyMain {
 
+
     public static void main(String[] args) {
-        ConcurrentHashMap<String, EvictingQueue<KlineVO>> testMap = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, ConcurrentHashMap<String, EvictingQueue<KlineVO>>> testMap = new ConcurrentHashMap<>();
 
-        long start = 0;
-        long end = 0;
-        // 先垃圾回收
-        System.gc();
-        start = Runtime.getRuntime().freeMemory();
-        IntStream.range(0, 4000).forEach(i -> {
-            String topic = "market.eosusdt.kline.1min" + i;
-            EvictingQueue<KlineVO> datas = EvictingQueue.create(2000);
-            KlineVO data = new KlineVO();
-            data.setId(1537325760L);
-            data.setOpen(5.1214);
-            data.setClose(5.1199);
-            data.setLow(5.117);
-            data.setHigh(5.1223);
-            data.setAmount(1947.6461);
-            data.setVol(9972.01690388);
-            data.setCount(18L);
+        KlineVO kl = new KlineVO();
+        kl.setId(1537325760L);
+        kl.setOpen(5.1214);
+        kl.setClose(5.1199);
+        kl.setLow(5.117);
+        kl.setHigh(5.1223);
+        kl.setAmount(1947.6461);
+        kl.setVol(9972.01690388);
+        kl.setCount(18L);
 
-            IntStream.range(0, 2000).forEach(j -> datas.add(data));
+        System.out.printf("sizeOf = %s M\n", (RamUsageEstimator.sizeOf(kl) * 400 * 10 * 2000) / (1024.0 * 1024));
 
-            testMap.put(topic, datas);
-        });
 
-        // 快要计算的时,再清理一次
-        System.gc();
-        end = Runtime.getRuntime().freeMemory();
-
-        System.out.println("占内存:" + (start - end) / 1000.0 + "M");
     }
 }
 
